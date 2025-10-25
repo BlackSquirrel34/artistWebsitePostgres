00593e5c508d902749f7b0af4d9601651a0c2128
BlackSquirrel <Maexxla3458@proton.me>
2025-10-25 22:23:38 +0200

    saved

--- website/src/app/(frontend)/layout.tsx
+++ website/src/app/(frontend)/layout.tsx
@@ -5,8 +5,8 @@ import Footer from '@/components/Footer'
 import FetchNavData, { fetchOwnerName } from '@/utils/fetchNavData'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'A website about the artist showcasing their works, biography, and news',
+  title: 'Artist Website',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
@@ -28,3 +28,5 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
     </html>
   )
 }
+
+// let's be careful: maybe there are no navdata yet
