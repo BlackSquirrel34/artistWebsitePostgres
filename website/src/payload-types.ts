789909407458b37766554b4aa137136c4111182d
BlackSquirrel <Maexxla3458@proton.me>
2025-11-12 10:18:53 +0100

    upgraded to payload version 3.60.0 to solve nixpacks error

--- website/src/payload-types.ts
+++ website/src/payload-types.ts
@@ -208,7 +208,7 @@ export interface Text {
       root: {
         type: string;
         children: {
-          type: string;
+          type: any;
           version: number;
           [k: string]: unknown;
         }[];
